e29cde240b3ee79b3f271d7d6e0655b5665b09a0
NaturezzZ <NaturezzZ@users.noreply.github.com>
2019-05-21 08:56:23 +0800

    Update picmaker.py

--- Train/picmaker.py
+++ Train/picmaker.py
@@ -5,6 +5,8 @@ import tables
 path = "E:\\0NaturezzZ\\SchoolWork\\2019Spring\\AI_Intro\\CV_Smallclass\\Project\\Inpainting"
 
 def makepic():
+	print('Preparing for ground truth, mask and ground truth with mask...')
+	present_path = os.getcwd()
 	os.chdir(path)
 	mask_path = "mask.hdf5"
 	pic_path = "pic.hdf5"
@@ -22,19 +24,15 @@ def makepic():
 	'''
 	起点是r*1000*256 终点是(r+1)*1000*256 -256
 	'''
-	
-	print(type(pic_file))
-	
+		
 	pic_data1 = pic_data[256000*r:256000*(r+1)]
 	pic_data1 = np.array(pic_data1)
 	pic_data1 = pic_data1.reshape(-1,256,256,3)
-	print(pic_data1.shape)
 
 	output[:, :, :, :3] = pic_data1[ :, :, :];
 	mask_data1 = mask_data[256000*randommask:256000*(randommask + 1)]
 	mask_data1 = np.array(mask_data1)
 	mask_data1 = mask_data1.reshape(-1,256,256,1)
-	print(mask_data1.shape)
 
 	output[:, :, :, 3] = mask_data1[:1000, :, :, 0]
 	output[:, :, :, 4] = mask_data1[:1000, :, :, 0]
@@ -62,5 +60,16 @@ def makepic():
 	mask_file.close()
 	pic_file.close()
 	#print(output)
+	os.chdir(present_path)
+	print('Preparing completed!')
 	return output
-
+#for testing
+'''
+make = makepic()
+gt = make[0,:,:,:3]
+mask = make[0,:,:,3:6]
+gt_mask = make[0,:,:,6:9]
+cv2.imwrite('gt.png',gt)
+cv2.imwrite('mask.png',mask)
+cv2.imwrite('gt_mask.png',gt_mask)
+'''
